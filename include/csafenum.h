779e7c7b396c6afa4e1d27dee48ea99fe1162c1d
Felix Palmen <felix@palmen-it.de>
2015-07-07 11:16:04 +0200

    bugs fixed

--- include/csafenum.h
+++ include/csafenum.h
@@ -10,9 +10,10 @@
 #define CSAFENUM_DECL(tname) struct tname##_struct; \
     typedef const struct tname##_struct * tname; \
     const char *tname##_name(tname e) __attribute__((nonnull(1))); \
-    int tname##_val(tname e) __attribute__((nonnull(1)));
+    int tname##_val(tname e) __attribute__((nonnull(1))); \
+    struct tname##_struct
 
-#define CSE_DECL(tname, name) extern const struct tname##_struct * const name;
+#define CSE_DECL(tname, name) extern const struct tname##_struct * const name
 
 #define CSAFENUM_DEF(tname) struct tname##_struct { \
     int val; \
@@ -22,15 +23,16 @@ const char *tname##_name(tname e) \
 { \
     assert(e); \
     return e->name; \
-}; \
+} \
 int tname##_val(tname e) \
 { \
     assert(e); \
     return e->val; \
-};
+} \
+struct tname##_struct
 
 #define CSE_DEF(tname, name, v) \
-    static const tname##_struct name##_memb { v, name }; \
-    const struct tname##_struct * const name = &name##_memb;
+    static const struct tname##_struct name##_memb = { v, #name }; \
+    const struct tname##_struct * const name = &name##_memb
 
 #endif
